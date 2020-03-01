f977c26d3f83bf031e928b7cdd3f86bdffa57808
nurandemir <nurum.demir@gmail.com>
2020-03-01 12:54:36 -0600

    basic command

---
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -0,0 +1,28 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) throws Exception{
+        //to start selenium script we need:
+        //setup webdriver(browser driver) and create webdriver object
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        //in Selenium, everything starts from WebDriver interface
+        driver.get("http://google.com");
+
+        Thread.sleep(3000);
+        String title=driver.getTitle();
+
+
+
+        driver.close();
+
+
+
+
+    }
+}
