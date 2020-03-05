d0355bf09bf6f819e7e7d167f1bbd4b187451eac
nurandemir <nurum.demir@gmail.com>
2020-03-05 11:00:25 -0600

    Find Elements by Using List

--- src/test/java/com/automation/tests/warmup/EbayPractice.java
+++ src/test/java/com/automation/tests/warmup/EbayPractice.java
@@ -13,9 +13,10 @@ public class EbayPractice {
         //click on search button
         //print number of results
 
-        WebDriverManager.chromedriver().version("79.0").setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.ebay.com/");
+
+WebDriverManager.chromedriver().setup();
+WebDriver driver=new ChromeDriver();
+
         driver.findElement(By.id("gh-ac")).sendKeys("selenium books" + Keys.ENTER);
         System.out.println(driver.findElement(By.tagName("h1")).getText().split(" ")[0]);
         driver.quit();
@@ -25,8 +26,8 @@ public class EbayPractice {
         //enter search term
         //click on search button
         //verify title contains search term
-        //driver.get("https://www.amazon.com/");
-        //driver.findElement(By.id("twotabsearchtextbox")).sendKeys("cookbook");
+        driver.get("https://www.amazon.com/");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("cookbook");
 
         WebDriverManager.chromedriver().version("79.0").setup();
         WebDriver driver1 = new ChromeDriver();
@@ -45,12 +46,12 @@ public class EbayPractice {
 
 
 
-        //Go to wikipedia.org
-        //enter search term `selenium webdriver`
-        //click on search button
-        //click on search result `Selenium (software)`
-        //verify url ends with `Selenium_(software)`
-
+//        //Go to wikipedia.org
+//        //enter search term `selenium webdriver`
+//        //click on search button
+//        //click on search result `Selenium (software)`
+//        //verify url ends with `Selenium_(software)`
+//
         WebDriverManager.chromedriver().version("79.0").setup();
         WebDriver driver2 = new ChromeDriver();
         driver2.get("https://www.wikipedia.com/");
@@ -67,5 +68,80 @@ public class EbayPractice {
 
         driver2.quit();
 
+        //Vasyl' way
+        //public class March4 {
+        //    static WebDriver driver;
+        //    public static void main(String[] args) throws Exception{
+        ////        ebayTest();
+        ////        amazonTest();
+        //        wikiTest();
+        //    }
+        //    /**
+        //     * Go to ebay --->          driver.get("http://ebay.com");
+        //     * enter search term        input.sendKeys("java book");
+        //     * click on search button   searchButton.click();
+        //     * print number of results
+        //     */
+        //    public static void ebayTest() throws Exception {
+        //        WebDriver driver = DriverFactory.createDriver("chrome");
+        //        driver.get("http://ebay.com");
+        //        Thread.sleep(2000);//to wait 2 seconds
+        //        driver.findElement(By.id("gh-ac")).sendKeys("java book");
+        //        Thread.sleep(2000);//to wait 2 seconds
+        //        driver.findElement(By.id("gh-btn")).click();
+        //        Thread.sleep(4000);//to wait 2 seconds
+        //        WebElement searchResults = driver.findElement(By.tagName("h1"));
+        //        String[] searchSentence = searchResults.getText().split(" ");
+        //        System.out.println(Arrays.toString(searchSentence));
+        //        System.out.println(searchSentence[0]);
+        //        driver.quit();
+        //    }
+        //    /**
+        //     * go to amazon
+        //     * enter search term
+        //     * click on search button
+        //     * verify title contains search term
+        //     */
+        //    public static void amazonTest() throws Exception{
+        //        driver = DriverFactory.createDriver("chrome");
+        //        driver.get("http://amazon.com");
+        //        //enter text and click ENTER
+        //        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("java book", Keys.ENTER);
+        //        Thread.sleep(4000);//to wait 2 seconds
+        //        String title = driver.getTitle();
+        //        if(title.contains("java book")){
+        //            System.out.println("TEST PASSED");
+        //        }else {
+        //            System.out.println("TEST FAILED");
+        //        }
+        //        driver.quit();
+        //    }
+        //    /**
+        //     * Go to wikipedia.org
+        //     * enter search term `selenium webdriver`
+        //     * click on search button
+        //     * click on search result `Selenium (software)`
+        //     * verify url ends with `Selenium_(software)`
+        //     */
+        //    public static void wikiTest() throws Exception{
+        //        driver = DriverFactory.createDriver("chrome");
+        //        //Go to wikipedia.org
+        //        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        //        //enter search term `selenium webdriver` & click on search button
+        //        driver.findElement(By.id("searchInput")).sendKeys("selenium webdriver", Keys.RETURN);
+        //        Thread.sleep(3000);
+        //        //click on search result `Selenium (software)`
+        //        driver.findElement(By.partialLinkText("Selenium (software)")).click();
+        //        Thread.sleep(2000);
+        //        String link = driver.getCurrentUrl(); // to get link as a String
+        //        //verify url ends with `Selenium_(software)`
+        //        if(link.endsWith("Selenium_(software)")){
+        //            System.out.println("TEST PASSED");
+        //        }else {
+        //            System.out.println("TEST FAILED");
+        //        }
+        //        driver.quit();
+        //    }
+        //}
     }
 }
--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class DriverFactory {
     public static WebDriver createDriver(String browserName) {
         if (browserName.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
+            WebDriverManager.chromedriver().version("79.0").setup();
             return new ChromeDriver();
         } else {
             WebDriverManager.firefoxdriver().setup();
